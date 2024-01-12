b8be4bbc1a36c358c6d237812909f159ce81a3ed
mrchanr <42151767+mrchanr@users.noreply.github.com>
2024-01-12 14:13:28 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,15 +1,13 @@
 
 
-public class GoogleBillboard {
-	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
+public class Main {
+	
 	public static void main(final String[] args) {
-		System.out.println("Not the right string, tests will fail!");
+		PigLatin sim = new PigLatin();
+        	sim.tester();
 	}
 	
 
-	//Finish this function
-	public boolean isPrime(){
-	}
 
 }
 
